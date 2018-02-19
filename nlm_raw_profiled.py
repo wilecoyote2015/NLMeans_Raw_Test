6c618bdc1a3bdcd86ff28400a55172e824be556f
Your Name <you@example.com>
2018-02-19 22:08:54 +0100

    changes

--- nlm_raw_profiled.py
+++ nlm_raw_profiled.py
@@ -5,17 +5,17 @@ from denoiser_nlm import Denoiser
 
 # settings
 num_cores = 4
-patch_radius = 5
+patch_radius = 3
 # h = 0.000005  # for Nikon 2
 # h = 0.00008  # for Nikon 1
 # h = 0.000008  # for Fuji
 h = 0.2  # for Fuji Dpreview
-num_balls_per_direction = 7
+num_balls_per_direction = 5
 
 
-slice_width = 1200
-slice_center_x = 1500
-slice_center_y = 1000
+slice_width = 300
+slice_center_x = 900
+slice_center_y = 1200
 
 x_min = int (slice_center_x - slice_width/2)
 x_max = int (slice_center_x + slice_width/2)
@@ -24,15 +24,19 @@ y_min = int (slice_center_y - slice_width/2)
 y_max = int (slice_center_y + slice_width/2)
 
 slice_denoise = np.s_[y_min:y_max, x_min:x_max]
-# slice_denoise = None
+slice_denoise = None
 
 # import image
-path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/nikon_2.NEF"
+# path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/nikon_3.NEF"
+# path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/nikon_2.NEF"
+path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/nikon_1.NEF"
 # path_output = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/result_nikon_1.png"
 # path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/Fuji_2.RAF"
 # path_output = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/result_fuji_2.png"
 # path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/Nikon_1_Profiled.RAF"
-path_output = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/Nikon_2_profiled_test.png"
+path_output = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/denoised_images/Nikon_1_full_pd{}_pr{}_h{}.png".format(num_balls_per_direction,
+                                                                                                                                 patch_radius,
+                                                                                                                                 h)
 path_profile = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/results/profile_D40.csv"
 
 image_raw = rawpy.imread(path_input)
--- profile_camera.py
+++ profile_camera.py
@@ -6,9 +6,10 @@ num_cores = 4
 patch_radius = 10
 
 # import image
-path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/Profiling_X_T20/DSCF7134.RAF"
+# path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/Profiling_X_T20/DSCF7134.RAF"
+path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/Profiling_D40/DSC_1577.NEF"
 dir_output = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/results"
-output_parameters = os.path.join(dir_output, "profile_T10_3200.csv")
+output_parameters = os.path.join(dir_output, "profile_D40_1600.csv")
 
 
 profiler = Profiler(patch_radius, num_cores=num_cores)
