5ad013b40a0a2661944408f59e03fbc5e4a20e40
Your Name <you@example.com>
2018-02-20 22:48:26 +0100

    save config

--- nlm_raw_profiled.py
+++ nlm_raw_profiled.py
@@ -11,12 +11,12 @@ patch_radius = 3
 # h = 0.000008  # for Fuji
 h = 0.2  # for Fuji Dpreview
 num_balls_per_direction = 5
-rotate_patches = False
+rotate_patches = True
 
 
-slice_width = 300
-slice_center_x = 1500
-slice_center_y = 1000
+slice_width = 100
+slice_center_x = 800
+slice_center_y = 1200
 
 x_min = int (slice_center_x - slice_width/2)
 x_max = int (slice_center_x + slice_width/2)
@@ -25,17 +25,17 @@ y_min = int (slice_center_y - slice_width/2)
 y_max = int (slice_center_y + slice_width/2)
 
 slice_denoise = np.s_[y_min:y_max, x_min:x_max]
-# slice_denoise = None
+slice_denoise = None
 
 # import image
 # path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/nikon_3.NEF"
 # path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/nikon_2.NEF"
-path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/nikon_3.NEF"
+path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/nikon_1.NEF"
 # path_output = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/result_nikon_1.png"
 # path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/Fuji_2.RAF"
 # path_output = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/result_fuji_2.png"
 # path_input = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/Nikon_1_Profiled.RAF"
-path_output = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/denoised_images/Nikon_3_crop_pd{}_pr{}_h{}.png".format(num_balls_per_direction,
+path_output = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/denoised_images/Nikon_1_full_rotated_pd{}_pr{}_h{}.png".format(num_balls_per_direction,
                                                                                                                                  patch_radius,
                                                                                                                                  h)
 path_profile = "/run/media/bjoern/daten/Programming/Raw_NLM_Denoise/images/results/profile_D40.csv"
@@ -47,6 +47,7 @@ denoiser = Denoiser(patch_radius, h, num_balls_per_direction, path_profile_camer
                     rotate_patches=rotate_patches)
 denoiser.filter_image(image_raw, slice_denoise)
 
-image_processed = image_raw.postprocess(output_bps=8, use_auto_wb=True)
+image_processed = image_raw.postprocess(output_bps=8, use_auto_wb=True,
+                                        demosaic_algorithm=rawpy.DemosaicAlgorithm.AMAZE)
 
 cv2.imwrite(path_output, np.flip(image_processed, axis=2))
